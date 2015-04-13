8c2e4a8750a692c05a3f92e871ec4fe3b46379ba
Adam Mawby <a.mawby@hotmail.com>
2015-04-13 15:53:46 -0400

    Adam's Changes to test
    1st commit - april 13

---
+++ Cirfull/Assets/Script/PlayTimer.js
@@ -0,0 +1,21 @@
+﻿
+import UnityEngine.UI;
+public var counterText: Text;
+var seconds: float;
+var minutes: float;
+
+
+function Start () {
+ 
+
+
+}
+
+function Update () {
+minutes = (Time.timeSinceLevelLoad/60f);
+seconds = (Time.timeSinceLevelLoad % 60f);
+
+
+counterText.text = minutes.ToString("00") + ": " + seconds.ToString("00");
+
+}
